8e1a0ebc0c65d87469c60536640daa14c23f3c3e
Robert <robert.ivaniszyn@gmail.com>
2016-11-18 10:25:17 -0600

    First draft of full user schema
    Filled in a lot of missing pieces with user schema so that we can create
    full users, whether they are currently looking to drive or looking to
    ride.

--- app/models/user.js
+++ app/models/user.js
@@ -1,46 +1,97 @@
 // app/models/user.js
 // load the things we need
 var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
+var bcrypt = require('bcrypt-nodejs');
+
+var nameObject = {
+	prefix : String,
+	first : String,
+	middle : String,
+	last : String,
+	suffix : String,
+};
+
+var addressObject = {
+		street : String,
+		city : String,
+		state : String,
+		zip : Number,
+};
+
+var endpointObject = {
+		location : String,
+		date : Date
+};
+
+var profileSchema = mongoose.Schema({
+	name : nameObject,
+	address : addressObject
+});
+
+var tripSchema = mongoose.Schema({
+	type : String,
+	start : endpointObject,
+	end : endpointObject,
+	options : {
+		seats : Number,
+		bags : Number,
+		additionalMileage : Number,
+		additionalTravelTime : Number,
+		pickupFee : Number,
+		budget : Number
+	},
+});
+
+var vehicleSchema = mongoose.Schema({
+	make : String,
+	model : String,
+	year : Number,
+
+});
 
 // define the schema for our user model
 var userSchema = mongoose.Schema({
 
-    local            : {
-        email        : String,
-        password     : String,
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    }
+	local : {
+		email : String,
+		password : String,
+	},
+	facebook : {
+		id : String,
+		token : String,
+		email : String,
+		name : String
+	},
+	twitter : {
+		id : String,
+		token : String,
+		displayName : String,
+		username : String
+	},
+	google : {
+		id : String,
+		token : String,
+		email : String,
+		name : String
+	},
+	hykeAccount : {
+		profile : profileSchema,
+		trips : [tripSchema],
+		vehicles : [vehicleSchema]
+	}
 
 });
 
 // methods ======================
 // generating a hash
 userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // checking if password is valid
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
+	return bcrypt.compareSync(password, this.local.password);
 };
 
 // create the model for users and expose it to our app
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema); 
--- server.js
+++ server.js
@@ -1,33 +1,54 @@
 /**
  * @author Robert T Ivaniszyn II
  */
+console.log('\n--------------------------------------------------');
+console.log("Welcome to the Hyke Server");
+console.log('--------------------------------------------------\n');
 
 //Declare a 'self' for the server for holding the master server object
 var self = this;
 
-// set up ======================================================================
-// get all the tools we need
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash = require('connect-flash');
-var engines = require('consolidate');
-
-var morgan = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-
-var configDB = require('./config/database.js');
-var configDefaults = require('./config/defaults.js');
-
 //Instantiate empty vars for dev requires
 var util;
 
+//load local configs
+var configDB = require('./config/database.js');
+var configDefaults = require('./config/defaults.js');
+
+// set up ======================================================================
+// get all the tools we need
+//Express
+var express = require('express');
+var session = require('express-session');
+var app = express();
+var port = process.env.PORT || 8080;
+
+//Mongoose
+var mongoose = require('mongoose');
 // Use native promises
 mongoose.Promise = global.Promise;
+//Load all relevant db schema into one object
+var dbSchema = {
+	configSchemas : require('./app/models/config'),
+	userSchema : require('./app/models/user')
+};
+// connect to our database
+mongoose.connect(configDB.url);
+
+//Passport
+var passport = require('passport');
+// pass passport for configuration
+require('./config/passport')(passport);
+
+//Flash Messages
+var flash = require('connect-flash');
+
+//Others
+var engines = require('consolidate');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var dev = require('./app/devTools');
 
 var serverState = {
 	dev : false, //default dev mode to off
@@ -41,67 +62,77 @@ process.argv.forEach(function(val, index, array) {
 	};
 });
 
-//Instantiate all relevant db schema into one object
-var dbSchema = {
-	configSchemas : require('./app/models/config'),
-	userSchema : require('./app/models/user')
-};
-
 self.server = {
 	state : serverState,
-	//configs :
+	configs : {
+		sessionConfig : configDefaults.sessionConfig
+	},
 	schema : dbSchema
 };
 
-// configuration ===============================================================
-mongoose.connect(configDB.url);
-// connect to our database
-
-require('./config/passport')(passport);
-// pass passport for configuration
-
 var serverInit = function(next) {
-	console.log("Hyke Server Initializing...");
+	console.log("Hyke Server Initializing...\n");
 
-	//create statics for public assets
-	app.use('/css', express.static(__dirname + '/public/css/'));
-	app.use('/themes', express.static(__dirname + '/themes/'));
-	app.use('/img', express.static(__dirname + '/themes/img'));
-	app.use('/js', express.static(__dirname + '/public/js/'));
-	app.use('/fonts', express.static(__dirname + '/public/fonts/'));
-	app.use('/npm', express.static(__dirname + '/node_modules/'));
-	app.use('/pages', express.static(__dirname + '/public/pages/'));
-	app.use('/privatePages', express.static(__dirname + '/private/pages'));
+	console.log("Loading configuration from DB...\n");
+	getDBConfigs(function(result) {
+		if (result['_id'] === null) {
+			self.server.configs = result;
+		};
 
-	// set up our express application
-	app.use(morgan('dev'));
-	// log every request to the console
-	app.use(cookieParser());
-	// read cookies (needed for auth)
-	app.use(bodyParser.urlencoded({
-		extended : true
-	}));
-	app.use(bodyParser.json());
-	// get information from html forms
+		console.log("Loading configuration from DB complete!\n");
 
-	// required for passport
-	app.use(session(configDefaults.sessionConfig));
-	// session secret
-	app.use(passport.initialize());
-	app.use(passport.session());
-	// persistent login sessions
-	app.use(flash());
+		console.log("Configuring Express for REST API...\n");
+		//create statics for public assets
+		app.use('/css', express.static(__dirname + '/public/css/'));
+		app.use('/themes', express.static(__dirname + '/themes/'));
+		app.use('/img', express.static(__dirname + '/themes/img'));
+		app.use('/js', express.static(__dirname + '/public/js/'));
+		app.use('/fonts', express.static(__dirname + '/public/fonts/'));
+		app.use('/npm', express.static(__dirname + '/node_modules/'));
+		app.use('/pages', express.static(__dirname + '/public/pages/'));
+		app.use('/privatePages', express.static(__dirname + '/private/pages'));
+		// set up our express application
+		app.use(morgan('dev'));
+		// log every request to the console
+		app.use(cookieParser());
+		// read cookies (needed for auth)
+		app.use(bodyParser.urlencoded({
+			extended : true
+		}));
+		app.use(bodyParser.json());
+		// get information from html forms
 
-	// use connect-flash for flash messages stored in session
+		console.log("Configuring passport for authentication...\n");
+		// required for passport
+		app.use(session(self.server.configs.sessionConfig));
+		// session secret
+		app.use(passport.initialize());
+		app.use(passport.session());
+		// persistent login sessions
+		app.use(flash());
+		// use connect-flash for flash messages stored in session
 
-	// routes ======================================================================
-	require('./app/routes.js')(app, express, passport);
-	// load our routes and pass in our app and fully configured passport
-	next();
+		console.log("Loading routes for API...\n");
+		// routes ======================================================================
+		require('./app/routes.js')(app, express, passport);
+		// load our routes and pass in our app and fully configured passport
+		
+		console.log('Hyke Server init complete!');
+		console.log('--------------------------------------------------\n');
+		next();
+
+	});
 
 };
 
-var dev = require('./app/devTools');
+var serverStart = function(next) {
+	console.log('\n--------------------------------------------------');
+	console.log("Starting Hyke Server...");
+	console.log('--------------------------------------------------\n');
+	// launch ======================================================================
+	app.listen(port);
+	next();
+};
 
 var getDBConfigs = function(callback) {
 	//Load the schema locally for ease of use
@@ -110,26 +141,34 @@ var getDBConfigs = function(callback) {
 	//Create a local instance of the schema
 	var sessionConfigSchema = configSchemas.sessionConfigSchema;
 
-	if (self.server.state.dev) dev.printSchema(sessionConfigSchema, "sessionConfigSchema");
+	if (self.server.state.dev)
+		dev.printSchema(sessionConfigSchema, "sessionConfigSchema");
 
 	sessionConfigSchema.findOne().sort({
 		'_id' : -1
-	}).exec(function(error, returnData) {
-		if (self.server.state.dev) dev.printReturnData(error, returnData);
+	}).lean().exec(function(error, returnData) {
+
+		if (error) {
+			console.log("Error getting session config data: " + error);
+			callback(error);
+		} else {
+			if (self.server.state.dev)
+				dev.printReturnData(error, returnData);
+			callback(returnData);
+		};
 
 	});
 };
 
-var serverStart = function(next) {
-	// launch ======================================================================
-	app.listen(port);
-	next();
-};
 
 serverInit(function() {
 	serverStart(function() {
-		console.log('The magic happens on port ' + port);
-		getDBConfigs();
+		console.log("Hyke Server has successfully started!\n");
+		console.log('--------------------------------------------------\n');
+		console.log('The magic happens on port ' + port + '!');
+		console.log('--------------------------------------------------');
+		console.log('Traffic Log:');
+		console.log('--------------------------------------------------\n');
 	});
 });
 
